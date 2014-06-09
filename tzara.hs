abcfbb12fe19c02f3127b298bf015943e978a743
Thomas Levine <_@thomaslevine.com>
2014-06-09 08:04:48 -0400

    haskell types

---
+++ tzara.hs
@@ -0,0 +1,24 @@
+import qualified Data.Map as M
+
+type Array = undefined
+
+data Cell = Column {
+  nchar :: Array Int,
+  nnewline :: Array Int,
+  nuppercase :: Array Int,
+  nlowercase :: Array Int,
+  ndigit :: Array Int,
+  nspace :: Array Int
+}
+
+type Column = [Cell]
+
+type Table = [Column]
+
+
+        'nchar': len(stripped),
+        'nnewline': stripped.count('\n'),
+        'nspace': stripped.count(' '),
+        'nuppercase': re.sub(notupper, '', stripped),
+        'nlowercase': re.sub(notlower, '', stripped),
+        'ndigit': re.sub(notdigit, '', stripped),
