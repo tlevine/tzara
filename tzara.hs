dbc076f03c83810f10dc36291f44ea7783fee97f
Thomas Levine <_@thomaslevine.com>
2014-06-09 09:57:51 -0400

    -

--- tzara.hs
+++ tzara.hs
@@ -1,18 +1,11 @@
 import qualified Data.Map as M
 import qualified Data.Char as C
 
-type Array = undefined
+data Feature = Nchar | Nnewline | Nuppercase | Nlowercase | Ndigit | Nspace
 
-data Cell = Cell {
-  nchar :: Array Int,
-  nnewline :: Array Int,
-  nuppercase :: Array Int,
-  nlowercase :: Array Int,
-  ndigit :: Array Int,
-  nspace :: Array Int
-}
+data Cell = M.Map Feature Int
 
-type Column = [Cell]
+type Column = M.Map Feature [Int]
 
 type Table = [Column]
 
@@ -22,14 +15,17 @@ tableDist :: Distance Table
 
 columnDist :: Distance Column
 
-fromCSVCell :: String -> Cell
-fromCSVCell raw = { nchar = length raw,
-                    nnewline = count (== '\n') raw,
-                    nspace = count (== ' )' raw,
-                    nuppercase = count C.isAsciiUpper raw
-                    nuppercase = count C.isAsciiLower raw,
-                    ndigit = count C.isNumber raw,
-                    nmark = count C.isMark raw,
-                    nsymbol = count C.isSymbol raw }
+toCell :: String -> Cell
+toCell raw = M.fromList $ [("nchar", length raw),
+                           ("nnewline", count (== '\n') raw),
+                           ("nspace", count (== ' )' raw),
+                           ("nuppercase", count C.isAsciiUpper raw
+                           ("nuppercase", count C.isAsciiLower raw),
+                           ("ndigit", count C.isNumber raw),
+                           ("nmark", count C.isMark raw),
+                           ("nsymbol", count C.isSymbol raw) ]
   where
     count func = foldr (\x sum -> if (func x) then sum + 1 else sum) 0
+
+toColumn :: [Cell] -> Column
+toColumn cell:cells
